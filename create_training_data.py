04fbc27cebb65e666cc0fc5c493e1df1911db49b
Onur Yesil <onuryesil@gmail.com>
2025-02-07 17:23:40 +0300

    Update get_arc_generator.py, create_training_data.py: - Added 6 new arc task generators (get_arc_generator.py). - Data creation is done with multiprocessing now (create_training_data.py).

--- create_training_data.py
+++ create_training_data.py
@@ -1,7 +1,6 @@
 import argparse
 from pathlib import Path
 from get_tokenizer import Tokenizer
-from tqdm.auto import tqdm
 import pickle
 import shutil
 import numpy as np
@@ -10,52 +9,49 @@ import os
 import random
 import json
 from get_arc_generator import ArcGenerator
+import multiprocessing as mp
+import functools
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--train_data_path", type=str, default="data/finetune")
-parser.add_argument("--val_data_path", type=str, default="data/finetune")
+parser.add_argument("--train_data_path", type=str, default="data/combined")
+parser.add_argument("--val_data_path", type=str, default="data/training")
 parser.add_argument("--processed_data_path", type=str, default="data/pretraining")
 parser.add_argument("--num_shards", type=int, default=10)
 parser.add_argument("--vocab_size", type=int, default=16)
 parser.add_argument("--num_workers", type=int, default=10)
-parser.add_argument(
-    "--tokenizer_save_path", type=str, default=""
-)
+parser.add_argument("--tokenizer_save_path", type=str, default="")
 parser.add_argument("--noise_epoch", type=int, default=5000)
 
 args = parser.parse_args()
 
 
 def create_data(
+    output_file_path,
     source_path,
     tokenizer,
-    output_file_path,
-    is_train=True,
-    rolled=True,
-    augmented=True,
-    add_noise=False,
+    is_train,
+    rolled,
+    augmented,
+    add_noise,
 ):
     data_path = Path(source_path)
     output_file_path = Path(output_file_path)
     filelist = os.listdir(data_path)
     augmentor = Augmentor()
-    
-    #Add data from actual arc tasks
+    arc_generator = ArcGenerator(num_repeat=3)
+
+    # Add data from actual arc tasks
     tasks = []
-    for file in filelist:
+    for i, file in enumerate(filelist):
         json_path = data_path / file
         with open(json_path, "r") as fhandle:
             task = json.load(fhandle)
         tasks.append(task)
+        if is_train and (i % 10 == 0):
+            generated_tasks = arc_generator()
+            tasks.extend(generated_tasks)
 
-    if is_train: 
-        #Add data from generated arc tasks
-        arc_generator = ArcGenerator()
-        generated_tasks = arc_generator(num_repeats=10)
-        #Combined the two
-        tasks.extend(generated_tasks)
-    
     data = []
     for task in tasks:
         if is_train:
@@ -78,6 +74,8 @@ def create_data(
 
     np.save(output_file_path, data)
 
+    return output_file_path
+
 
 if __name__ == "__main__":
     tokenizer = Tokenizer(args.vocab_size)
@@ -89,22 +87,23 @@ if __name__ == "__main__":
         shutil.rmtree(PROCESSED_DATA_PATH)
 
     print("Training data is being created.")
-    for i in tqdm(range(1, args.num_shards + 1)):
+    train_create_data = functools.partial(
+        create_data,
+        source_path=TRAIN_DATA_PATH,
+        tokenizer=tokenizer,
+        is_train=True,
+        rolled=True,
+        augmented=True,
+        add_noise=False,
+    )
 
-        if i > args.noise_epoch:
-            add_noise = bool(i % 2)
-        else:
-            add_noise = False
+    training_file_paths = (
+        PROCESSED_DATA_PATH / f"training_{i}.npy" for i in range(1, args.num_shards + 1)
+    )
 
-        create_data(
-            source_path=TRAIN_DATA_PATH,
-            tokenizer=tokenizer,
-            output_file_path=PROCESSED_DATA_PATH / f"training_{i}.npy",
-            is_train=True,
-            rolled=True,
-            augmented=True,
-            add_noise=add_noise,
-        )
+    with mp.Pool(args.num_workers) as pool:
+        for outfile_path in pool.imap(train_create_data, training_file_paths):
+            print(f'File {outfile_path} is created.')
 
     # Validation data is being created
     create_data(
@@ -114,6 +113,7 @@ if __name__ == "__main__":
         is_train=False,
         rolled=False,
         augmented=False,
+        add_noise=True,
     )
 
     if args.tokenizer_save_path:
--- get_arc_generator.py
+++ get_arc_generator.py
@@ -12,8 +12,11 @@ def trim_array(array, *, left=0, right=0, up=0, down=0):
     return array[up:down, left:right]
 
 
-def random_rectangle_zeros(max_size=8):
-    n_rows, n_cols = random.randint(2, max_size), random.randint(2, max_size)
+def random_rectangle_zeros(min_size=2, max_size=8):
+    n_rows, n_cols = (
+        random.randint(min_size, max_size),
+        random.randint(min_size, max_size),
+    )
     return np.zeros(shape=(n_rows, n_cols))
 
 
@@ -36,116 +39,289 @@ def random_location(n_rows, n_cols):
     return random.randint(0, n_rows - 1), random.randint(0, n_cols - 1)
 
 
+def impute_diagonal(array, start_position: tuple[int, int], direction):
+    if direction == "ne":
+        sums = (-1, 1)
+    elif direction == "sw":
+        sums = (1, -1)
+    elif direction == "nw":
+        sums = (-1, -1)
+    elif direction == "se":
+        sums = (1, 1)
+    else:
+        raise ValueError
+    i, j = start_position
+    while True:
+        try:
+            i, j = i + sums[0], j + sums[1]
+            if i < 0 or j < 0:
+                break
+            array[i, j] = 1
+        except IndexError:
+            break
+
+
 # ARC task generators starts from this line
 def rect_encap(num_repeat, max_rec_size=(8, 8), max_padding=5):
-    examples = []
-    for _ in range(num_repeat):
-        n_rows, n_cols = (
-            random.randint(1, max_rec_size[0]),
-            random.randint(1, max_rec_size[1]),
-        )
-        output_array = np.ones(shape=(n_rows, n_cols))
-        # pad the output_array to get input array
-        input_array, num_pad = random_pad(output_array, max_padding)
-        # roll the output array
-        input_array, shifts = random_roll(input_array, num_pad)
-        examples.append(
-            {"input": input_array.tolist(), "output": output_array.tolist()}
-        )
-    return examples
+    while True:
+        examples = []
+        for _ in range(num_repeat):
+            n_rows, n_cols = (
+                random.randint(1, max_rec_size[0]),
+                random.randint(1, max_rec_size[1]),
+            )
+            output_array = np.ones(shape=(n_rows, n_cols))
+            # pad the output_array to get input array
+            input_array, num_pad = random_pad(output_array, max_padding)
+            # roll the output array
+            input_array, shifts = random_roll(input_array, num_pad)
+            examples.append(
+                {"input": input_array.tolist(), "output": output_array.tolist()}
+            )
+        yield examples
 
 
 def rect_encap_v2(num_repeat, max_rec_size=(8, 8), max_padding=5):
-    examples = []
-    for _ in range(num_repeat):
-        n_rows, n_cols = (
-            random.randint(1, max_rec_size[0]),
-            random.randint(1, max_rec_size[1]),
-        )
-        # This one has random output array
-        output_array = np.random.randint(low=1, high=10, size=(n_rows, n_cols))
-        # pad the output_array to get input array
-        input_array, num_pad = random_pad(output_array, max_padding)
-        # roll the output array
-        input_array, shifts = random_roll(input_array, num_pad)
-        examples.append(
-            {"input": input_array.tolist(), "output": output_array.tolist()}
-        )
-    return examples
+    while True:
+        examples = []
+        for _ in range(num_repeat):
+            n_rows, n_cols = (
+                random.randint(1, max_rec_size[0]),
+                random.randint(1, max_rec_size[1]),
+            )
+            # This one has random output array
+            output_array = np.random.randint(low=1, high=10, size=(n_rows, n_cols))
+            # pad the output_array to get input array
+            input_array, num_pad = random_pad(output_array, max_padding)
+            # roll the output array
+            input_array, shifts = random_roll(input_array, num_pad)
+            examples.append(
+                {"input": input_array.tolist(), "output": output_array.tolist()}
+            )
+        yield examples
 
 
 def fill_rectangle(num_repeat):
-    examples = []
-    for _ in range(num_repeat):
-        zeros = random_rectangle_zeros()
-        input_array = np.pad(zeros, pad_width=1, constant_values=1)
-        output_array = np.where(input_array == 0, 2, input_array)
-        input_array, num_pad = random_pad(input_array, max_padding=5)
-        input_array, shifts = random_roll(input_array, max_shift=num_pad)
-        output_array = np.pad(output_array, pad_width=num_pad)
-        output_array = np.roll(output_array, shift=shifts, axis=(0, 1))
-        examples.append(
-            {"input": input_array.tolist(), "output": output_array.tolist()}
-        )
-    return examples
+    while True:
+        examples = []
+        for _ in range(num_repeat):
+            zeros = random_rectangle_zeros()
+            input_array = np.pad(zeros, pad_width=1, constant_values=1)
+            output_array = np.where(input_array == 0, 2, input_array)
+            input_array, num_pad = random_pad(input_array, max_padding=5)
+            input_array, shifts = random_roll(input_array, max_shift=num_pad)
+            output_array = np.pad(output_array, pad_width=num_pad)
+            output_array = np.roll(output_array, shift=shifts, axis=(0, 1))
+            examples.append(
+                {"input": input_array.tolist(), "output": output_array.tolist()}
+            )
+        yield examples
 
 
 def vertical_lines(num_repeat):
-    examples = []
-    for _ in range(num_repeat):
-        input_array = random_rectangle_zeros(max_size=10)
-        n_rows, n_cols = input_array.shape
-        num_lines = random.randint(1, n_cols - 1)
-        js = []
-        for _ in range(num_lines):
-            i, j = random_location(n_rows, n_cols)
-            js.append(j)
-            input_array[i, j] = 1
-        output_array = np.copy(input_array)
-        for j in js:
-            output_array[:, j] = 1
-        examples.append(
-            {"input": input_array.tolist(), "output": output_array.tolist()}
-        )
-    return examples
+    while True:
+        examples = []
+        for _ in range(num_repeat):
+            input_array = random_rectangle_zeros(max_size=10)
+            n_rows, n_cols = input_array.shape
+            num_lines = random.randint(1, n_cols - 1)
+            js = []
+            for _ in range(num_lines):
+                i, j = random_location(n_rows, n_cols)
+                js.append(j)
+                input_array[i, j] = 1
+            output_array = np.copy(input_array)
+            for j in js:
+                output_array[:, j] = 1
+            examples.append(
+                {"input": input_array.tolist(), "output": output_array.tolist()}
+            )
+        yield examples
 
 
 def horizontal_lines(num_repeat):
-    examples = []
-    for _ in range(num_repeat):
-        input_array = random_rectangle_zeros(max_size=10)
-        n_rows, n_cols = input_array.shape
-        num_lines = random.randint(1, n_rows - 1)
-        rows = []
-        for _ in range(num_lines):
-            i, j = random_location(n_rows, n_cols)
-            rows.append(i)
-            input_array[i, j] = 1
-        output_array = np.copy(input_array)
-        for i in rows:
-            output_array[i, :] = 1
-        examples.append(
-            {"input": input_array.tolist(), "output": output_array.tolist()}
-        )
-    return examples
+    while True:
+        examples = []
+        for _ in range(num_repeat):
+            input_array = random_rectangle_zeros(max_size=10)
+            n_rows, n_cols = input_array.shape
+            num_lines = random.randint(1, n_rows - 1)
+            rows = []
+            for _ in range(num_lines):
+                i, j = random_location(n_rows, n_cols)
+                rows.append(i)
+                input_array[i, j] = 1
+            output_array = np.copy(input_array)
+            for i in rows:
+                output_array[i, :] = 1
+            examples.append(
+                {"input": input_array.tolist(), "output": output_array.tolist()}
+            )
+        yield examples
+
+
+def draw_diagonal_1(num_repeat, min_size=4, max_size=20):
+    while True:
+        examples = []
+        for _ in range(num_repeat):
+            input_array = random_rectangle_zeros(min_size=min_size, max_size=max_size)
+            ratio = 0.05
+            num_changes = int(ratio * input_array.size)
+            num_changes = num_changes if num_changes else 1
+            locations = []
+            for _ in range(num_changes):
+                i, j = random_location(*input_array.shape)
+                locations.append((i, j))
+                input_array[i, j] = 1
+            output_array = np.copy(input_array)
+            for location in locations:
+                impute_diagonal(output_array, location, "ne")
+                impute_diagonal(output_array, location, "sw")
+            examples.append(
+                {"input": input_array.tolist(), "output": output_array.tolist()}
+            )
+        yield examples
+
+
+def draw_diagonal_2(num_repeat, min_size=4, max_size=20):
+    while True:
+        examples = []
+        for _ in range(num_repeat):
+            input_array = random_rectangle_zeros(min_size=min_size, max_size=max_size)
+            ratio = 0.05
+            num_changes = int(ratio * input_array.size)
+            num_changes = num_changes if num_changes else 1
+            locations = []
+            for _ in range(num_changes):
+                i, j = random_location(*input_array.shape)
+                locations.append((i, j))
+                input_array[i, j] = 1
+            output_array = np.copy(input_array)
+            for location in locations:
+                impute_diagonal(output_array, location, "nw")
+                impute_diagonal(output_array, location, "se")
+            examples.append(
+                {"input": input_array.tolist(), "output": output_array.tolist()}
+            )
+        yield examples
+
+
+def draw_xs(num_repeat, min_size=4, max_size=20):
+    while True:
+        examples = []
+        for _ in range(num_repeat):
+            input_array = random_rectangle_zeros(min_size=min_size, max_size=max_size)
+            ratio = 0.05
+            num_changes = int(ratio * input_array.size)
+            num_changes = num_changes if num_changes else 1
+            locations = []
+            for _ in range(num_changes):
+                i, j = random_location(*input_array.shape)
+                locations.append((i, j))
+                input_array[i, j] = 1
+            output_array = np.copy(input_array)
+            for location in locations:
+                impute_diagonal(output_array, location, "ne")
+                impute_diagonal(output_array, location, "sw")
+                impute_diagonal(output_array, location, "nw")
+                impute_diagonal(output_array, location, "se")
+            examples.append(
+                {"input": input_array.tolist(), "output": output_array.tolist()}
+            )
+        yield examples
+
+
+def color_mapping_columns(num_repeat, min_size=3, max_size=8):
+    colors = list(range(1, 10))
+    random.shuffle(colors)
+    mappings = {colors.pop(): colors.pop() for i in range(4)}
+    for k, v in tuple(mappings.items()):
+        mappings[v] = k
+    mappings[colors[-1]] = colors[-1]
+    while True:
+        examples = []
+        for _ in range(num_repeat):
+            input_array = random_rectangle_zeros(min_size, max_size)
+            output_array = np.copy(input_array)
+            n_cols = input_array.shape[1]
+            for j in range(n_cols):
+                input_color = random.randint(1, 9)
+                input_array[:, j] = input_color
+                output_array[:, j] = mappings[input_color]
+            examples.append(
+                {"input": input_array.tolist(), "output": output_array.tolist()}
+            )
+        yield examples
+
+
+def color_mapping_rows(num_repeat, min_size=3, max_size=8):
+    colors = list(range(1, 10))
+    random.shuffle(colors)
+    mappings = {colors.pop(): colors.pop() for i in range(4)}
+    for k, v in tuple(mappings.items()):
+        mappings[v] = k
+    mappings[colors[-1]] = colors[-1]
+    while True:
+        examples = []
+        for _ in range(num_repeat):
+            input_array = random_rectangle_zeros(min_size, max_size)
+            output_array = np.copy(input_array)
+            n_rows = input_array.shape[0]
+            for i in range(n_rows):
+                input_color = random.randint(1, 9)
+                input_array[i, :] = input_color
+                output_array[i, :] = mappings[input_color]
+            examples.append(
+                {"input": input_array.tolist(), "output": output_array.tolist()}
+            )
+        yield examples
+
+
+def color_mapping_dots(num_repeat, min_size=4, max_size=10):
+    colors = list(range(1, 10))
+    random.shuffle(colors)
+    mappings = {colors.pop(): colors.pop() for i in range(4)}
+    for k, v in tuple(mappings.items()):
+        mappings[v] = k
+    mappings[colors[-1]] = colors[-1]
+    while True:
+        examples = []
+        for _ in range(num_repeat):
+            input_array = random_rectangle_zeros(min_size, max_size)
+            output_array = np.copy(input_array)
+            n_dots = int(0.2 * input_array.size)
+            n_dots = n_dots if n_dots else 1
+            for i in range(n_dots):
+                input_color = random.randint(1, 9)
+                i, j = random_location(*input_array.shape)
+                input_array[i, j] = input_color
+                output_array[i, j] = mappings[input_color]
+            examples.append(
+                {"input": input_array.tolist(), "output": output_array.tolist()}
+            )
+        yield examples
 
 
 class ArcGenerator:
-    def __init__(self):
+    def __init__(self, num_repeat: int | Iterable[int]):
         self.task_generators = [
-            rect_encap,
-            rect_encap_v2,
-            fill_rectangle,
-            vertical_lines,
-            horizontal_lines,
+            rect_encap(num_repeat),
+            rect_encap_v2(num_repeat),
+            fill_rectangle(num_repeat),
+            vertical_lines(num_repeat),
+            horizontal_lines(num_repeat),
+            draw_diagonal_1(num_repeat),
+            draw_diagonal_2(num_repeat),
+            draw_xs(num_repeat),
+            color_mapping_columns(num_repeat=4),
+            color_mapping_rows(num_repeat=4),
+            color_mapping_dots(num_repeat=4),
         ]
 
-    def __call__(self, num_repeats: int | Iterable[int]):
-        if not isinstance(num_repeats, Iterable):
-            num_repeats = (num_repeats,) * len(self.task_generators)
-
+    def __call__(self):
         tasks = []
-        for num_repeat, task_generator in zip(num_repeats, self.task_generators):
-            tasks.append({"train": task_generator(num_repeat)})
+        for task_generator in self.task_generators:
+            tasks.append({"train": next(task_generator)})
 
+        random.shuffle(tasks)
         return tasks
